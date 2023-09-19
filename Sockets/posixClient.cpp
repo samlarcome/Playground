116933c33cab68d18a05c3894cc54d8399fdf5ff
Samuel Larcome <87437514+samlarcome@users.noreply.github.com>
2023-09-18 22:01:55 -0400

    Updated POSIX Client to use structs

--- Sockets/posixClient.cpp
+++ Sockets/posixClient.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,51 +8,53 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
+#define SERVER_ADDR "localhost"
+#define SERVER_PORT "49160"
+
+using namespace std;
+
+void handle_socket_error(string error_message) {
+    cout << error_message << "\n";
+    exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+    int ai_result, connect_result, send_result;
+    int connect_socket;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+    struct addrinfo *ll_result, *ptr, hints;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    ai_result = getaddrinfo(SERVER_ADDR, SERVER_PORT, &hints, &ll_result);
+    if (ai_result != 0) { handle_socket_error("Error Getting Address Information"); }
+
+    for(ptr = ll_result; ptr != NULL; ptr = ptr->ai_next) {
+        connect_socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (connect_socket == -1) { handle_socket_error("Error Creating Connecting Socket"); }
+
+        connect_result = connect(connect_socket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (connect_result == -1) { 
+            close(connect_socket);
+            connect_socket = -1;
+            continue;
+        }
+
+        break;
     }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+
+    freeaddrinfo(ll_result);
+
+    if (connect_socket == -1) {
+        handle_socket_error("Cannot Connect to Server");
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n", buffer);
-    close(sockfd);
-    return 0;
+
+    send_result = send(connect_socket, "Hello World From Client!\n", 24, 0);
+
+
+    close(connect_socket);
 }
